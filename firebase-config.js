12afbba6d0f8c72c3b2d5c0fb9f9f0b9a2420320
sweet1039rin-cyber <sweet1039rin@gmail.com>
2026-08-14 13:52:28 +0000

    Revert "Add Firestore check sync"
    This reverts commit 744f50798b9c591f65e4f195cc4c1fe6780d9a39.

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +17,5 @@ import { getFirestore, doc, getDoc, setDoc, onSnapshot } from "https://www.gstat
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
-window.stepUpFirebase = { app, auth, db, doc, getDoc, setDoc, onSnapshot, signInWithEmailAndPassword, onAuthStateChanged, signOut };
+window.stepUpFirebase = { app, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut };
 window.dispatchEvent(new Event("stepUpFirebaseReady"));
